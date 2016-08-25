2a5629092114f4aa4f7493a8a986db5b0f63449b
James Dinsdale <hi@molovo.co.uk>
2016-08-25 23:05:31 +0100

    Add short parameters for colors

--- color.zsh
+++ color.zsh
@@ -1,19 +1,19 @@
 #!/usr/bin/env zsh
 
 function color() {
-  local color=$1
+  local color=$1 style=$2 b=0
 
   shift
 
   case $color in
-    black)    echo "\033[0;30m${@}\033[0;m" ;;
-    red)      echo "\033[0;31m${@}\033[0;m" ;;
-    green)    echo "\033[0;32m${@}\033[0;m" ;;
-    yellow)   echo "\033[0;33m${@}\033[0;m" ;;
-    blue)     echo "\033[0;34m${@}\033[0;m" ;;
-    magenta)  echo "\033[0;35m${@}\033[0;m" ;;
-    cyan)     echo "\033[0;36m${@}\033[0;m" ;;
-    white)    echo "\033[0;37m${@}\033[0;m" ;;
+    black|b)    echo "\033[${b};30m${@}\033[0;m" ;;
+    red|r)      echo "\033[${b};31m${@}\033[0;m" ;;
+    green|g)    echo "\033[${b};32m${@}\033[0;m" ;;
+    yellow|y)   echo "\033[${b};33m${@}\033[0;m" ;;
+    blue|bl)    echo "\033[${b};34m${@}\033[0;m" ;;
+    magenta|m)  echo "\033[${b};35m${@}\033[0;m" ;;
+    cyan|c)     echo "\033[${b};36m${@}\033[0;m" ;;
+    white|w)    echo "\033[${b};37m${@}\033[0;m" ;;
   esac
 }
 
